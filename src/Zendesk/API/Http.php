a76e79c5353ff0b4c804ffaadf3f559bdcc9b793
Mike Boulet <mikeboulet@hundredacreweb.com>
2014-05-13 11:37:55 -0400

    Fixed issue where API call would fail if per_page or page parameters were used

--- src/Zendesk/API/Http.php
+++ src/Zendesk/API/Http.php
@@ -60,7 +60,7 @@ class Http {
             curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
             curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
         } else {
-            $curl = curl_init($url.($json != (object) null ? '?'.http_build_query($json) : ''));
+            $curl = curl_init($url.($json != (object) null ? (strpos($url, '?') === false ? '?' : '&').http_build_query($json) : ''));
             curl_setopt($curl, CURLOPT_CUSTOMREQUEST, ($method ? $method : 'GET'));
         }
         if($client->getAuthType() == 'oauth_token') {
