0b8ffb29e46868a1ed6b7760b83acf91ba29132f
Joey Rivera <joeyrivera@air-watch.com>
2014-07-14 17:17:23 -0400

    Need to remove @ since we are loading the file in HTTP::send instead of passing that value as a parameter to curl. Else fopen keeps breaking and we are unable to upload an attachment.

--- src/Zendesk/API/Attachments.php
+++ src/Zendesk/API/Attachments.php
@@ -33,7 +33,7 @@ class Attachments extends ClientAbstract {
         } 
         
         $endPoint = Http::prepare('uploads.json?filename='.$params['name'].(isset($params['optional_token']) ? '&token='.$params['optional_token'] : ''));
-        $response = Http::send($this->client, $endPoint, array('filename' => '@'.$params['file']), 'POST', (isset($params['type']) ? $params['type'] : 'application/binary'));
+        $response = Http::send($this->client, $endPoint, array('filename' => $params['file']), 'POST', (isset($params['type']) ? $params['type'] : 'application/binary'));
        if ((!is_object($response)) || ($this->client->getDebug()->lastResponseCode != 201)) {
             throw new ResponseException(__METHOD__);
         }
