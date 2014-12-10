907cb057a990f69135158b885eaca992b8202dd8
Yeu-Shuan Tang <ytang@zendesk.com>
2014-12-10 15:33:26 -0800

    Add class BasicTest for test class constructor

---
+++ client_config.php
@@ -0,0 +1,8 @@
+<?php
+
+putenv("SUBDOMAIN=z3n521");
+putenv("USERNAME=ytang@zendesk.com");
+putenv("PASSWORD=123456");
+putenv("TOKEN=***REMOVED***");
+
+?>
---
+++ tests/Zendesk/API/Tests/BasicTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Zendesk\API\Tests;
+
+include("client_config.php");
+
+use Zendesk\API\Client;
+
+/**
+ * Basic test class
+ */
+class BasicTest extends \PHPUnit_Framework_TestCase {
+
+    protected $client;
+    protected $subdomain;
+    protected $password;
+    protected $token;
+    protected $oAuthToken;
+
+    public function __construct() {
+        $this->subdomain = getenv('SUBDOMAIN');
+        $this->username = getenv('USERNAME');
+        $this->password = getenv('PASSWORD');
+        $this->token = getenv('TOKEN');
+        $this->oAuthToken = getenv('OAUTH_TOKEN');
+        $this->client = new Client($this->subdomain, $this->username);
+        $this->client->setAuth('token', $this->token);
+    }
+}
+
+?>
