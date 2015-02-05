d3168c5120d6f13105f3c231db4ad9a52ff574b3
Lazaro de La Torre <jpete369@gmail.com>
2015-02-04 18:29:08 -0600

    removed extra whitespace

--- tests/Zendesk/API/UnitTests/GroupsTest.php
+++ tests/Zendesk/API/UnitTests/GroupsTest.php
@@ -9,7 +9,7 @@ use Zendesk\API\Client;
  */
 class GroupsTest extends BasicTest {
 
-    public function setUP() {
+	public function setUP() {
 		// Create Group Mock Object
 		$group_mock_object = new \stdClass();
 		$group_mock_object->group = new \stdClass();
@@ -21,10 +21,10 @@ class GroupsTest extends BasicTest {
 		$this->group = $group_mock_object;
     }
     
-    public function create() {
-        // Test for Create Method
-   		$this->mock->expects($this->any())->method('create')->will($this->returnValue($this->returnArgument(0)));
-		$group = $this->mock->create($this->group);
+	public function create() {
+		// Test for Create Method
+		$this->mock->expects($this->any())->method('create')->will($this->returnValue($this->returnArgument(0)));
+   		$group = $this->mock->create($this->group);
         
 		$this->assertEquals(is_object($group), true, 'Should return an object');
 		$this->assertEquals(is_object($group->group), true, 'Should return an object called "group"');
@@ -32,53 +32,53 @@ class GroupsTest extends BasicTest {
 		$this->assertEquals($group->group->name, 'New Group', 'Name of test group does not match');
     }
 
-    public function testAll() {
+	public function testAll() {
 		// Test for FindAll Method
-    	$this->mock->expects($this->any())->method('findAll')->will($this->returnValue($this->group)); 
-        $groups = $this->mock->findAll();
-        
-        $this->assertEquals(is_object($groups), true, 'Should return an object');
-        $this->assertEquals(is_object($groups->group), true, 'Should return an object called "group"');
-        $this->assertGreaterThan(0, $groups->group->id, 'Returns a non-numeric id for groups[0]');
+		$this->mock->expects($this->any())->method('findAll')->will($this->returnValue($this->group)); 
+		$groups = $this->mock->findAll();
+
+		$this->assertEquals(is_object($groups), true, 'Should return an object');
+		$this->assertEquals(is_object($groups->group), true, 'Should return an object called "group"');
+		$this->assertGreaterThan(0, $groups->group->id, 'Returns a non-numeric id for groups[0]');
     }
 
-    public function testAssignable() {
+	public function testAssignable() {
 		// Test for FindAll Method with 'assignable' parameter
-    	$this->mock->expects($this->any())->method('findAll')->will($this->returnValue($this->group));
-        $groups = $this->mock->findAll(array('assignable' => true));
+		$this->mock->expects($this->any())->method('findAll')->will($this->returnValue($this->group));
+		$groups = $this->mock->findAll(array('assignable' => true));
 
-        $this->assertEquals(is_object($groups), true, 'Should return an object');
-        $this->assertEquals(is_object($groups->group), true, 'Should return an object containing an array called "groups"');
-        $this->assertGreaterThan(0, $groups->group->id, 'Returns a non-numeric id for groups[0]');
+		$this->assertEquals(is_object($groups), true, 'Should return an object');
+		$this->assertEquals(is_object($groups->group), true, 'Should return an object containing an array called "groups"');
+		$this->assertGreaterThan(0, $groups->group->id, 'Returns a non-numeric id for groups[0]');
     }
 
-    public function testFind() {
+	public function testFind() {
 		// Test for Find Method
-    	$this->mock->expects($this->any())->method('find')->will($this->returnValue($this->group));
-        $group = $this->mock->find($this->group->group->id);
-        
-        $this->assertEquals(is_object($group), true, 'Should return an object');
-        $this->assertGreaterThan(0, $group->group->id, 'Returns a non-numeric id for group');
+		$this->mock->expects($this->any())->method('find')->will($this->returnValue($this->group));
+		$group = $this->mock->find($this->group->group->id);
+
+		$this->assertEquals(is_object($group), true, 'Should return an object');
+		$this->assertGreaterThan(0, $group->group->id, 'Returns a non-numeric id for group');
     }
 
-    public function testUpdate() {
+	public function testUpdate() {
 		// Test for Update Method
-    	$this->mock->expects($this->any())->method('update')->will($this->returnArgument(0));
+		$this->mock->expects($this->any())->method('update')->will($this->returnArgument(0));
 		$this->group->group->name = "New Group II";
-        $group = $this->mock->update($this->group);
+		$group = $this->mock->update($this->group);
         
-        $this->assertEquals(is_object($group), true, 'Should return an object');
-        $this->assertEquals(is_object($group->group), true, 'Should return an object called "group"');
-        $this->assertGreaterThan(0, $group->group->id, 'Returns a non-numeric id for group');
-        $this->assertEquals($group->group->name, 'New Group II', 'Name of test group does not match');
+		$this->assertEquals(is_object($group), true, 'Should return an object');
+		$this->assertEquals(is_object($group->group), true, 'Should return an object called "group"');
+		$this->assertGreaterThan(0, $group->group->id, 'Returns a non-numeric id for group');
+		$this->assertEquals($group->group->name, 'New Group II', 'Name of test group does not match');
     }
 
-    public function tearDown() {
+	public function tearDown() {
 		// Test for Delete Method
-    	$this->mock->expects($this->any())->method('delete')->will($this->returnValue(null));
-    	$group = $this->mock->delete($this->group->group->id);
+		$this->mock->expects($this->any())->method('delete')->will($this->returnValue(null));
+		$group = $this->mock->delete($this->group->group->id);
 
-        $this->assertEquals(null, $group, 'Does not return a null object');
+		$this->assertEquals(null, $group, 'Does not return a null object');
     }
 
 }
