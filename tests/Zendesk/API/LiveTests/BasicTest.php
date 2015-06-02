01882ee906ad4257d82edcb3ae188d73852c37c8
Mio Miguel Galang <mgalang@zendesk.com>
2015-06-02 16:36:30 +0800

    Moved parameters to an options array so we only override the options we need

--- tests/Zendesk/API/LiveTests/BasicTest.php
+++ tests/Zendesk/API/LiveTests/BasicTest.php
@@ -35,16 +35,20 @@ abstract class BasicTest extends \PHPUnit_Framework_TestCase
         $this->client->setAuth('token', $this->token);
     }
 
-    protected function mockApiCall($httpMethod, $path, $response, $code = 200, $timesCalled = 1)
+    protected function mockApiCall($httpMethod, $path, $response, $options = array())
     {
+        $options = array_merge(array(
+            'code' => 200,
+            'timesCalled' => 1
+        ), $options);
         $this->http->mock
-            ->exactly($timesCalled)
+            ->exactly($options['timesCalled'])
             ->when()
                 ->methodIs($httpMethod)
                 ->pathIs('/api/v2' . $path)
             ->then()
                 ->body(json_encode($response))
-                ->statusCode($code)
+                ->statusCode($options['code'])
             ->end();
         $this->http->setUp();
     }
