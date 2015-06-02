01b979115806cfb723bcdfe37c7e50f26f897092
Mio Miguel Galang <mgalang@zendesk.com>
2015-06-02 17:11:40 +0800

    Mock API calls for the ticket audit tests

--- tests/Zendesk/API/LiveTests/TicketAuditsTest.php
+++ tests/Zendesk/API/LiveTests/TicketAuditsTest.php
@@ -2,69 +2,82 @@
 
 namespace Zendesk\API\LiveTests;
 
-use Zendesk\API\Client;
-
 /**
  * Ticket Audits test class
  */
 class TicketAuditsTest extends BasicTest
 {
-
-    public function testCredentials()
-    {
-        parent::credentialsTest();
-    }
-
-    public function testAuthToken()
-    {
-        parent::authTokenTest();
-    }
-
     protected $ticket_id;
 
     public function setUp()
     {
         $testTicket = array(
+            'id' => '12345',
             'subject' => 'The quick brown fox jumps over the lazy dog',
             'comment' => array(
                 'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
             ),
             'priority' => 'normal'
         );
-        $ticket = $this->client->tickets()->create($testTicket);
+//        $ticket = $this->client->tickets()->create($testTicket);
+//
+//        $update_testTicket['id'] = $ticket->ticket->id;
+//        $update_testTicket2['subject'] = 'Updated subject';
+//        $update_testTicket2['priority'] = 'urgent';
+//        $ticket = $this->client->tickets()->update($update_testTicket);
 
-        $update_testTicket['id'] = $ticket->ticket->id;
-        $update_testTicket2['subject'] = 'Updated subject';
-        $update_testTicket2['priority'] = 'urgent';
-        $ticket = $this->client->tickets()->update($update_testTicket);
+        $this->ticket_id = $testTicket['id'];
 
-        $this->ticket_id = $ticket->ticket->id;
-
+        parent::setUp();
     }
 
     public function testAll()
     {
+        $this->mockApiCall('GET', '/tickets/12345/audits.json?',
+          array(
+            'audits' => array(
+                array(
+                    'id' => '1'
+                )
+            )
+          )
+        );
+
         $audits = $this->client->ticket($this->ticket_id)->audits()->findAll();
         $this->assertEquals(is_object($audits), true, 'Should return an object');
         $this->assertEquals(is_array($audits->audits), true,
             'Should return an object containing an array called "audits"');
         $this->assertGreaterThan(0, $audits->audits[0]->id, 'Returns a non-numeric id in first audit');
-        $this->assertEquals($this->client->getDebug()->lastResponseCode, '200', 'Does not return HTTP code 200');
     }
 
     public function testAllSideLoadedMethod()
     {
+        $this->mockApiCall('GET', '/tickets/12345/audits.json?include=users%2Cgroups&',
+          array(
+            'audits' => array(),
+            'users' => array(),
+            'groups' => array(),
+          )
+        );
+
         $audits = $this->client->ticket($this->ticket_id)->sideload(array('users', 'groups'))->audits()->findAll();
         $this->assertEquals(is_object($audits), true, 'Should return an object');
         $this->assertEquals(is_array($audits->users), true,
             'Should return an object containing an array called "users"');
         $this->assertEquals(is_array($audits->groups), true,
             'Should return an object containing an array called "groups"');
-        $this->assertEquals($this->client->getDebug()->lastResponseCode, '200', 'Does not return HTTP code 200');
     }
 
     public function testAllSideLoadedParameter()
     {
+        $this->mockApiCall('GET', '/tickets/12345/audits.json?include=users%2Cgroups&',
+          array(
+            'audits' => array(),
+            'users' => array(),
+            'groups' => array(),
+          )
+        );
+
         $audits = $this->client->ticket($this->ticket_id)->audits()->findAll(array(
             'sideload' => array(
                 'users',
@@ -76,18 +89,33 @@ class TicketAuditsTest extends BasicTest
             'Should return an object containing an array called "users"');
         $this->assertEquals(is_array($audits->groups), true,
             'Should return an object containing an array called "groups"');
-        $this->assertEquals($this->client->getDebug()->lastResponseCode, '200', 'Does not return HTTP code 200');
     }
 
     public function testFind()
     {
+        $this->mockApiCall('GET', '/tickets/12345/audits.json?',
+          array(
+            'audits' => array(
+              array(
+                'id' => '1'
+              )
+            )
+          )
+        );
         $audit_id = $this->client->ticket($this->ticket_id)->audits()->findAll()->audits[0]->id;
+
+        $this->mockApiCall('GET', '/tickets/12345/audits/1.json?',
+          array(
+            'audit' => array(
+              'id' => '1'
+            )
+          )
+        );
         $audits = $this->client->ticket($this->ticket_id)->audit($audit_id)->find();
         $this->assertEquals(is_object($audits), true, 'Should return an object');
         $this->assertEquals(is_object($audits->audit), true,
             'Should return an object containing an array called "audit"');
         $this->assertEquals($audit_id, $audits->audit->id, 'Returns an incorrect id in audit object');
-        $this->assertEquals($this->client->getDebug()->lastResponseCode, '200', 'Does not return HTTP code 200');
     }
 
     /*
@@ -97,10 +125,5 @@ class TicketAuditsTest extends BasicTest
     //     $audits = $this->client->ticket(2)->audit(16317679361)->markAsTrusted();
     //     $this->assertEquals($this->client->getDebug()->lastResponseCode, '200', 'Does not return HTTP code 200');
     // }
-
-    public function tearDown()
-    {
-        $this->client->tickets($this->ticket_id)->delete();
-    }
 
 }
