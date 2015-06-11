fec4df587f72983dee7fbe1e1dbe6cdd67197a91
Jose Consador <joseconsador@gmail.com>
2015-06-11 16:57:43 +0800

    use getLatestChainedParameter() to get the actual last chain.

--- src/Zendesk/API/UtilityTraits/ChainedParametersTrait.php
+++ src/Zendesk/API/UtilityTraits/ChainedParametersTrait.php
@@ -89,4 +89,15 @@ trait ChainedParametersTrait
         return $params;
     }
 
+    /**
+     * Returns the named chained parameter
+     *
+     * @return array
+     */
+    public function getLatestChainedParameter()
+    {
+        $chainedParameters = $this->getChainedParameters();
+
+        return array_slice($chainedParameters, -1);
+    }
 }
