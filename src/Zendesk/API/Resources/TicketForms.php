3ceaf8d819f01587477236b23fc290e4c73360a3
Jose Consador <joseconsador@gmail.com>
2015-06-15 16:47:16 +0800

    add TicketForms resource and test

---
+++ src/Zendesk/API/Resources/TicketForms.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Zendesk\API\Resources;
+
+use Zendesk\API\Exceptions\MissingParametersException;
+use Zendesk\API\Exceptions\ResponseException;
+use Zendesk\API\Http;
+
+class TicketForms extends ResourceAbstract
+{
+    const OBJ_NAME = 'ticket_forms';
+    const OBJ_NAME_PLURAL = 'ticket_forms';
+
+    protected $resourceName = 'ticket_forms';
+
+    protected function setUpRoutes()
+    {
+        parent::setUpRoutes();
+
+        $this->setRoutes([
+            'clone'   => 'ticket_forms/{id}/clone.json',
+            'reorder' => 'ticket_forms/reorder.json'
+        ]);
+    }
+
+    /**
+     * Clones an existing ticket form (can't use 'clone' as method name)
+     *
+     * @param array $params
+     *
+     * @throws MissingParametersException
+     * @throws ResponseException
+     * @throws Zendesk\API\Exceptions\MissingParametersException
+     *
+     * @return mixed
+     */
+    function cloneForm($id = null)
+    {
+        $class = get_class($this);
+        if (empty($id)) {
+            $id = $this->getChainedParameter($class);
+        }
+
+        if (empty($id)) {
+            throw new MissingParametersException(__METHOD__, ['id']);
+        }
+
+        $response = Http::send_with_options(
+            $this->client,
+            $this->getRoute('clone', ['id' => $id]),
+            ['method' => 'POST']
+        );
+
+        $this->client->setSideload(null);
+
+        return $response;
+    }
+
+    /**
+     * Reorder Ticket forms
+     *
+     * @param array $ticketFormIds
+     *
+     * @throws ResponseException
+     * @throws \Exception
+     *
+     * @return mixed
+     */
+    function reorder(array $ticketFormIds)
+    {
+        $response = Http::send_with_options(
+            $this->client,
+            $this->getRoute(__FUNCTION__),
+            ['postFields' => ['ticket_form_ids' => $ticketFormIds], 'method' => 'PUT']
+        );
+
+        if ($this->client->getDebug()->lastResponseCode != 200) {
+            throw new ResponseException(__METHOD__);
+        }
+
+        $this->client->setSideload(null);
+
+        return $response;
+    }
+}
---
+++ tests/Zendesk/API/UnitTests/TicketFormsTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Zendesk\API\UnitTests;
+
+/**
+ * Ticket Audits test class
+ */
+class TicketFormsTest extends BasicTest
+{
+    /**
+     * @expectedException Zendesk\API\Exceptions\ResponseException
+     */
+    public function testDeleteThrowsException()
+    {
+        $this->mockApiCall('DELETE', 'ticket_forms/1.json', [], ['statusCode' => 422]);
+        $this->client->tickets()->forms(1)->delete();
+    }
+
+    public function testCloneForm()
+    {
+        $this->mockApiCall('POST', 'ticket_forms/1/clone.json', [], ['statusCode' => 200]);
+        $this->client->tickets()->forms(1)->cloneForm();
+        $this->httpMock->verify();
+    }
+
+    /**
+     * Tests if an exception is thrown when a ticket form ID could not be retrieved from
+     * the method call.
+     *
+     * @expectedException Zendesk\API\Exceptions\MissingParametersException
+     */
+    public function testCloneFormThrowsException()
+    {
+        $this->mockApiCall('POST', 'ticket_forms/1/clone.json', [], ['statusCode' => 200]);
+        $this->client->tickets(1)->forms()->cloneForm();
+        $this->httpMock->verify();
+    }
+
+    public function testReorder()
+    {
+        $this->mockApiCall('PUT', 'ticket_forms/reorder.json', [], [
+                'statusCode' => 200,
+                'bodyParams' => ['ticket_form_ids' => [3, 4, 5, 1]]
+            ]
+        );
+        $this->client->tickets()->forms()->reorder([3, 4, 5, 1]);
+        $this->httpMock->verify();
+    }
+}
