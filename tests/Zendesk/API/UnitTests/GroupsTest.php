ec7f0d288557c362c700e0dc2079545a11bdf032
Jose Consador <joseconsador@gmail.com>
2015-06-29 15:58:37 +0800

    add groups resource and unit test

--- tests/Zendesk/API/UnitTests/GroupsTest.php
+++ tests/Zendesk/API/UnitTests/GroupsTest.php
@@ -19,4 +19,35 @@ class GroupsTest extends BasicTest
             'endpoint' => 'groups/assignable.json',
         ]);
     }
+
+    public function testFindUserGroups()
+    {
+        $this->mockAPIResponses([
+            new Response(200, [], '')
+        ]);
+
+        $this->client->users(123)->groups()->findAll();
+
+        $this->assertLastRequestIs([
+            'method'   => 'GET',
+            'endpoint' => 'users/123/groups.json',
+        ]);
+    }
+
+    /**
+     * Tests if the default findAll route is still accessible
+     */
+    public function testFindAllGroups()
+    {
+        $this->mockAPIResponses([
+            new Response(200, [], '')
+        ]);
+
+        $this->client->groups()->findAll();
+
+        $this->assertLastRequestIs([
+            'method'   => 'GET',
+            'endpoint' => 'groups.json',
+        ]);
+    }
 }
