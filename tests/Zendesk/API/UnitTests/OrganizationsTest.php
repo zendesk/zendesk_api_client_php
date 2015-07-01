350b25372ab9c004d45f6dd7c4c14cd449f24201
Jose Consador <joseconsador@gmail.com>
2015-07-01 16:35:02 +0800

    Add organisations endpoint and test

---
+++ tests/Zendesk/API/UnitTests/OrganizationsTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Zendesk\API\UnitTests;
+
+use GuzzleHttp\Psr7\Response;
+
+class OrganizationsTest extends BasicTest
+{
+    public function testFindUserOrganizations()
+    {
+        $this->mockAPIResponses([
+            new Response(200, [], '')
+        ]);
+
+        $this->client->users(123)->organizations()->findAll();
+
+        $this->assertLastRequestIs([
+            'method'   => 'GET',
+            'endpoint' => 'users/123/organizations.json',
+        ]);
+    }
+
+    /**
+     * Tests if the default findAll route is still accessible
+     */
+    public function testFindAllOrganizations()
+    {
+        $this->mockAPIResponses([
+            new Response(200, [], '')
+        ]);
+
+        $this->client->organizations()->findAll();
+
+        $this->assertLastRequestIs([
+            'method'   => 'GET',
+            'endpoint' => 'organizations.json',
+        ]);
+    }
+
+    public function testAutocomplete()
+    {
+        $this->mockAPIResponses([
+            new Response(200, [], '')
+        ]);
+
+        $this->client->organizations()->autocomplete('foo');
+
+        $this->assertLastRequestIs([
+            'method'      => 'GET',
+            'endpoint'    => 'organizations/autocomplete.json',
+            'queryParams' => ['name' => 'foo']
+        ]);
+    }
+
+    public function testRelated()
+    {
+        $this->mockAPIResponses([
+            new Response(200, [], '')
+        ]);
+
+        $this->client->organizations()->related(123);
+
+        $this->assertLastRequestIs([
+            'method'   => 'GET',
+            'endpoint' => 'organizations/123/related.json'
+        ]);
+    }
+
+    public function testSearchByExternalId()
+    {
+        $this->mockAPIResponses([
+            new Response(200, [], '')
+        ]);
+
+        $this->client->organizations()->search(123);
+
+        $this->assertLastRequestIs([
+            'method'      => 'GET',
+            'endpoint'    => 'organizations/search.json',
+            'queryParams' => ['external_id' => 123]
+        ]);
+    }
+}
