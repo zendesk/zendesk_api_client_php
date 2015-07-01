0566d5f3982b6beedbe31bf765fe5e981aef9b10
Mio Miguel Galang <mgalang@zendesk.com>
2015-07-01 11:06:51 +0800

    Improve logic for getting name from file path. Add test for uploading with no name specified

--- src/Zendesk/API/Resources/Attachments.php
+++ src/Zendesk/API/Resources/Attachments.php
@@ -15,10 +15,10 @@ class Attachments extends ResourceAbstract
     protected function setUpRoutes()
     {
         $this->setRoutes([
-          'upload'       => "uploads.json",
-          'deleteUpload' => "uploads/{token}.json",
-          'delete'       => "{$this->resourceName}/{id}.json",
-          'find'         => "{$this->resourceName}/{id}.json",
+            'upload'       => "uploads.json",
+            'deleteUpload' => "uploads/{token}.json",
+            'delete'       => "{$this->resourceName}/{id}.json",
+            'find'         => "{$this->resourceName}/{id}.json",
         ]);
     }
 
@@ -40,22 +40,17 @@ class Attachments extends ResourceAbstract
      */
     public function upload(array $params)
     {
-        if (! $this->hasKeys($params, array('file'))) {
-            throw new MissingParametersException(__METHOD__, array('file'));
-        }
-        if (! file_exists($params['file'])) {
+        if (! $this->hasKeys($params, ['file'])) {
+            throw new MissingParametersException(__METHOD__, ['file']);
+        } elseif (! file_exists($params['file'])) {
             throw new CustomException('File ' . $params['file'] . ' could not be found in ' . __METHOD__);
         }
-        if (! $params['name'] && strrpos($params['file'], '/') > - 1) {
-            $path_array     = explode('/', $params['file']);
-            $file_index     = count($path_array) - 1;
-            $params['name'] = $path_array[$file_index];
-        }
-        if (! $params['name'] && strrpos($params['file'], '/') == false) {
-            $params['name'] = $params['file'];
+
+        if (! isset($params['name'])) {
+            $params['name'] = basename($params['file']);
         }
 
-        $queryParams = ['filename' => $params ['name']];
+        $queryParams = ['filename' => $params['name']];
         if (isset($params['token'])) {
             $queryParams['token'] = $params['token'];
         }
@@ -64,10 +59,10 @@ class Attachments extends ResourceAbstract
             $this->client,
             $this->getRoute(__FUNCTION__),
             [
-            'method'      => 'POST',
-            'contentType' => 'application/binary',
-            'file'        => $params['file'],
-            'queryParams' => $queryParams,
+                'method'      => 'POST',
+                'contentType' => 'application/binary',
+                'file'        => $params['file'],
+                'queryParams' => $queryParams,
             ]
         );
 
@@ -90,7 +85,7 @@ class Attachments extends ResourceAbstract
     {
         $response = Http::sendWithOptions(
             $this->client,
-            $this->getRoute(__FUNCTION__, array('token' => $token)),
+            $this->getRoute(__FUNCTION__, ['token' => $token]),
             ['method' => 'DELETE']
         );
 
