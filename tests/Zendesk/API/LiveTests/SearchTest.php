147921cd392bdf4c569b0e07da0db6ef66627403
Jose Consador <joseconsador@gmail.com>
2015-07-03 15:14:50 +0800

    add unit and live test

--- tests/Zendesk/API/LiveTests/SearchTest.php
+++ tests/Zendesk/API/LiveTests/SearchTest.php
@@ -4,27 +4,10 @@ namespace Zendesk\API\LiveTests;
 
 class SearchTest extends BasicTest
 {
-    /**
-     * @dataProvider basicQueryStrings
-     */
-    public function testSearchQueryString($searchString)
+    public function testSearchQueryString()
     {
-        $queryParams = ['sort_by' => 'updated_at'];
-        $response    = $this->client->search()->find($searchString, $queryParams);
+        $response = $this->client->search()->find('type:ticket status:open', ['sort_by' => 'updated_at']);
 
-        var_dump($response);
-        exit;
-    }
-
-    public function basicQueryStrings()
-    {
-        return [
-            //[3245227],
-            //['Greenbriar'],
-            ['type:user "Jane Doe"'],
-            //['type:ticket status:open'],
-            //['type:organization created<2015-05-01'],
-            //['created>2012-07-17 type:ticket organization:"MD Photo"'],
-        ];
+        $this->assertTrue(isset($response->results), 'Should contain a property called `results`');
     }
 }
