0f9e2951b687f628ed39680408f6bce9af9597be
Jose Consador <joseconsador@gmail.com>
2015-07-08 13:58:07 +0800

    Remove use of DefaultsMany

--- tests/Zendesk/API/UnitTests/DummyResource.php
+++ tests/Zendesk/API/UnitTests/DummyResource.php
@@ -2,8 +2,11 @@
 namespace Zendesk\API\UnitTests;
 
 use Zendesk\API\Resources\ResourceAbstract;
+use Zendesk\API\Traits\Resource\CreateMany;
 use Zendesk\API\Traits\Resource\Defaults;
-use Zendesk\API\Traits\Resource\DefaultsMany;
+use Zendesk\API\Traits\Resource\DeleteMany;
+use Zendesk\API\Traits\Resource\FindMany;
+use Zendesk\API\Traits\Resource\UpdateMany;
 
 /**
  * Class DummyResource
@@ -14,5 +17,8 @@ class DummyResource extends ResourceAbstract
     const OBJ_NAME_PLURAL = 'dummies';
 
     use Defaults;
-    use DefaultsMany;
+    use FindMany;
+    use CreateMany;
+    use UpdateMany;
+    use DeleteMany;
 }
