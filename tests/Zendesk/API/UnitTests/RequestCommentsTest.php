c21fcac4dc2d972942e4b6bae709e29e2920cfcd
Mio Miguel Galang <mgalang@zendesk.com>
2015-07-13 15:11:40 +0800

    Update request comments test to use new utility method

--- tests/Zendesk/API/UnitTests/RequestCommentsTest.php
+++ tests/Zendesk/API/UnitTests/RequestCommentsTest.php
@@ -2,8 +2,6 @@
 
 namespace Zendesk\API\UnitTests;
 
-use GuzzleHttp\Psr7\Response;
-
 /**
  * Requests test class
  */
@@ -11,38 +9,20 @@ class RequestCommentsTest extends BasicTest
 {
     public function testFindAll()
     {
-        $this->mockAPIResponses([
-            new Response(200, [], '')
-        ]);
-
         $requestId = 3838;
 
-        $this->client->requests($requestId)->comments()->findAll();
-
-        $this->assertLastRequestIs(
-            [
-                'method'   => 'GET',
-                'endpoint' => "requests/{$requestId}/comments.json",
-            ]
-        );
+        $this->assertEndpointCalled(function () use ($requestId) {
+            $this->client->requests($requestId)->comments()->findAll();
+        }, "requests/{$requestId}/comments.json");
     }
 
     public function testFind()
     {
-        $this->mockAPIResponses([
-            new Response(200, [], '')
-        ]);
-
         $resourceId = 3838;
         $requestId  = 19192;
 
-        $this->client->requests($requestId)->comments()->find($resourceId);
-
-        $this->assertLastRequestIs(
-            [
-                'method'   => 'GET',
-                'endpoint' => "requests/{$requestId}/comments/{$resourceId}.json",
-            ]
-        );
+        $this->assertEndpointCalled(function () use ($requestId, $resourceId) {
+            $this->client->requests($requestId)->comments()->find($resourceId);
+        }, "requests/{$requestId}/comments/{$resourceId}.json");
     }
 }
