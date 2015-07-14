29019d360321a0b7edb4df2eca31ae05c4a35af9
Jose Consador <joseconsador@gmail.com>
2015-07-14 16:38:35 +0800

    Update tests to use utility method for testing endpoints

--- tests/Zendesk/API/UnitTests/UserIdentitiesTest.php
+++ tests/Zendesk/API/UnitTests/UserIdentitiesTest.php
@@ -2,7 +2,6 @@
 
 namespace Zendesk\API\UnitTests;
 
-use GuzzleHttp\Psr7\Response;
 use Zendesk\API\Resources\UserIdentities;
 
 /**
@@ -11,6 +10,9 @@ use Zendesk\API\Resources\UserIdentities;
 class UserIdentitiesTest extends BasicTest
 {
 
+    /**
+     * Tests if the unique routes are called correctly
+     */
     public function testRoutes()
     {
         $userId = 3124;
@@ -62,12 +64,11 @@ class UserIdentitiesTest extends BasicTest
         );
     }
 
+    /**
+     * Tests if the client can POST to the identities end point
+     */
     public function testCreateAsEndUser()
     {
-
-        $this->mockAPIResponses([
-            new Response(200, [], '')
-        ]);
         $userId = 3124;
 
         $postFields = [
@@ -75,70 +76,47 @@ class UserIdentitiesTest extends BasicTest
             'value' => 'thecustomer@domain.com'
         ];
 
-        $this->client->users($userId)->identities()->createAsEndUser($postFields);
-
-        $this->assertLastRequestIs(
-            [
-                'method'     => 'POST',
-                'endpoint'   => "end_users/{$userId}/identities.json",
-                'postFields' => [UserIdentities::OBJ_NAME => $postFields]
-            ]
-        );
+        $this->assertEndpointCalled(function () use ($userId, $postFields) {
+            $this->client->users($userId)->identities()->createAsEndUser($postFields);
+        }, "end_users/{$userId}/identities.json", 'POST', ['postFields' => [UserIdentities::OBJ_NAME => $postFields]]);
     }
 
+    /**
+     * Tests if the client can call and build the verify endpoint
+     */
     public function testVerify()
     {
-        $this->mockAPIResponses([
-            new Response(200, [], '')
-        ]);
         $userId = 3124;
         $id     = 123;
 
-        $this->client->users($userId)->identities($id)->verify();
-
-        $this->assertLastRequestIs(
-            [
-                'method'   => 'PUT',
-                'endpoint' => "users/{$userId}/identities/{$id}/verify.json",
-            ]
-        );
+        $this->assertEndpointCalled(function () use ($userId, $id) {
+            $this->client->users($userId)->identities($id)->verify();
+        }, "users/{$userId}/identities/{$id}/verify.json", 'PUT');
     }
 
+    /**
+     * Tests if the client can call and build the make primary endpoint
+     */
     public function testMakePrimary()
     {
-        $this->mockAPIResponses([
-            new Response(200, [], '')
-        ]);
-
         $userId = 3124;
         $id     = 123;
 
-        $this->client->users($userId)->identities($id)->makePrimary();
-
-        $this->assertLastRequestIs(
-            [
-                'method'   => 'PUT',
-                'endpoint' => "users/$userId/identities/$id/make_primary.json",
-            ]
-        );
+        $this->assertEndpointCalled(function () use ($id, $userId) {
+            $this->client->users($userId)->identities($id)->makePrimary();
+        }, "users/$userId/identities/$id/make_primary.json", 'PUT');
     }
 
+    /**
+     * Tests if the client can call and build the request verification endpoint
+     */
     public function testRequestVerification()
     {
-        $this->mockAPIResponses([
-            new Response(200, [], '')
-        ]);
-
         $userId = 3124;
         $id     = 123;
 
-        $this->client->users($userId)->identities($id)->requestVerification();
-
-        $this->assertLastRequestIs(
-            [
-                'method'   => 'PUT',
-                'endpoint' => "users/{$userId}/identities/{$id}/request_verification.json",
-            ]
-        );
+        $this->assertEndpointCalled(function () use ($id, $userId) {
+            $this->client->users($userId)->identities($id)->requestVerification();
+        }, "users/{$userId}/identities/{$id}/request_verification.json", 'PUT');
     }
 }
