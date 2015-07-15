5d73fb8a590c511ed33fba69199ce73f491a35b3
Mio Miguel Galang <mgalang@zendesk.com>
2015-07-15 14:20:44 +0800

    Add the sharing agreements resource

---
+++ src/Zendesk/API/Resources/SharingAgreements.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Zendesk\API\Resources;
+
+use Zendesk\API\Traits\Resource\FindAll;
+
+/**
+ * The SharingAgreements class
+ * https://developer.zendesk.com/rest_api/docs/core/sharing_agreements
+ */
+class SharingAgreements extends ResourceAbstract
+{
+    use FindAll;
+}
