85188692bf743e5d83181e98273eac382aaf50a4
Jose Consador <joseconsador@gmail.com>
2015-09-22 13:26:06 +0800

    Use guzzle's exception message

--- src/Zendesk/API/Http.php
+++ src/Zendesk/API/Http.php
@@ -85,7 +85,8 @@ class Http
             list ($request, $requestOptions) = $client->getAuth()->prepareRequest($request, $requestOptions);
             $response = $client->guzzle->send($request, $requestOptions);
         } catch (RequestException $e) {
-            throw new ApiResponseException($e);
+            $exception = RequestException::create($e->getRequest(), $e->getResponse());
+            throw new ApiResponseException($exception);
         } finally {
             $client->setDebug(
                 $request->getHeaders(),
