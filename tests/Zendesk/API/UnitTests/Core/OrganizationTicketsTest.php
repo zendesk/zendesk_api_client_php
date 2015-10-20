bca807d8355022ceb29190b33ced76e291a42d2b
Sven Buis <s.buis@moxx-mobility.com>
2015-10-20 13:56:03 +0200

    Added the possibility to load tickets for a specific organization

---
+++ tests/Zendesk/API/UnitTests/Core/OrganizationTicketsTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Zendesk\API\UnitTests\Core;
+
+use Zendesk\API\UnitTests\BasicTest;
+
+/**
+ * Organization Tickets test class
+ */
+class OrganizationTicketsTest extends BasicTest
+{
+    /**
+     * Test findAll method
+     */
+    public function testAll()
+    {
+
+        $organizationId = 1234;
+        $this->assertEndpointCalled(function () use ($organizationId) {
+            $this->client->organizations($organizationId)->tickets()->findAll();
+        }, "organizations/{$organizationId}/tickets.json");
+
+    }
+}
