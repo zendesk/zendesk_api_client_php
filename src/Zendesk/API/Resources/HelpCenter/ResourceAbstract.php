fb1d3ae457ffd77c91b5a062efa4d6624899d95d
Jose Mari Ramos <ramosjosemari@gmail.com>
2016-05-24 16:15:46 +0800

    Pull out generic methods on helpdesk

--- src/Zendesk/API/Resources/HelpCenter/ResourceAbstract.php
+++ src/Zendesk/API/Resources/HelpCenter/ResourceAbstract.php
@@ -18,4 +18,31 @@ abstract class ResourceAbstract extends \Zendesk\API\Resources\ResourceAbstract
 
         return 'help_center/' . $resourceName;
     }
+
+    /**
+     * Generate a route depending on a localization set
+     * @param string $name
+     * @param array $params
+     */
+    public function getRoute($name, array $params = [])
+    {
+        $routesWithLocale = ['findAll', 'find', 'create', 'update'];
+
+        $locale = $this->getLocale();
+        $resourceName = parent::getResourceNameFromClass();
+
+        if (in_array($name, $routesWithLocale) && isset($locale)) {
+            $originalResourceName = $this->resourceName;
+            $this->resourceName   = "help_center/{$locale}/" . $resourceName;
+
+            $route = parent::getRoute($name, $params);
+
+            // Reset resourceName so it doesn't affect succeeding calls
+            $this->resourceName = $originalResourceName;
+
+            return $route;
+        } else {
+            return parent::getRoute($name, $params);
+        }
+    }
 }
---
+++ src/Zendesk/API/Traits/Resource/Localize.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Zendesk\API\Traits\Resource;
+
+trait Localize
+{
+    /**
+     * @return string
+     */
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    /**
+     * @param string $locale
+     */
+    public function setLocale($locale)
+    {
+        if (is_string($locale)) {
+            $this->locale = $locale;
+        }
+
+        return $this;
+    }
+
+}
