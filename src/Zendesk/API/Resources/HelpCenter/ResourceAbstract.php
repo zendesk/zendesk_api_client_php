6fb6ae038e8b5349c738632cacd6bb055b1ae470
Jose Mari Ramos <ramosjosemari@gmail.com>
2016-05-25 10:51:55 +0800

    Pull out resourcename generation to trait

--- src/Zendesk/API/Resources/HelpCenter/ResourceAbstract.php
+++ src/Zendesk/API/Resources/HelpCenter/ResourceAbstract.php
@@ -2,47 +2,17 @@
 
 namespace Zendesk\API\Resources\HelpCenter;
 
+use Zendesk\API\Traits\Resource\ResourceName;
+
 /**
  * Abstract class for HelpCenter resources
  */
 abstract class ResourceAbstract extends \Zendesk\API\Resources\ResourceAbstract
 {
+    use ResourceName;
 
     /**
-     * Appends the help_center/ prefix to resource names
-     * @return string
-     */
-    protected function getResourceNameFromClass()
-    {
-        $resourceName = parent::getResourceNameFromClass();
-
-        return 'help_center/' . $resourceName;
-    }
-
-    /**
-     * Generate a route depending on a localization set
-     * @param string $name
-     * @param array $params
-     */
-    public function getRoute($name, array $params = [])
-    {
-        $routesWithLocale = ['findAll', 'find', 'create', 'update'];
-
-        $locale = $this->getLocale();
-        $resourceName = parent::getResourceNameFromClass();
-
-        if (in_array($name, $routesWithLocale) && isset($locale)) {
-            $originalResourceName = $this->resourceName;
-            $this->resourceName   = "help_center/{$locale}/" . $resourceName;
-
-            $route = parent::getRoute($name, $params);
-
-            // Reset resourceName so it doesn't affect succeeding calls
-            $this->resourceName = $originalResourceName;
-
-            return $route;
-        } else {
-            return parent::getRoute($name, $params);
-        }
-    }
+     * @var $prefix
+     **/
+    protected $prefix = 'help_center/';
 }
