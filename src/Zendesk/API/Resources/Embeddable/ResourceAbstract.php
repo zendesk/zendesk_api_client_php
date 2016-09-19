26e134d7bb204338f2a9feb5a74e4ffc915382dc
Sam Gavinio <samgavinio@gmail.com>
2016-09-19 08:54:48 +0800

    Create embeddable config_sets resource

---
+++ src/Zendesk/API/Resources/Embeddable/ResourceAbstract.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Zendesk\API\Resources\Embeddable;
+
+use Zendesk\API\Traits\Resource\ResourceName;
+
+/**
+ * Abstract class for Embeddable resources
+ */
+abstract class ResourceAbstract extends \Zendesk\API\Resources\ResourceAbstract
+{
+    use ResourceName;
+
+    /**
+     * @var string
+     **/
+    protected $prefix = '../../embeddable/api/';
+}
