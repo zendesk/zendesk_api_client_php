77759220710655d4476b3b65d014c3e39d4f3a0b
Glenn Hermans <glhermans@gmail.com>
2016-11-03 07:18:33 +0100

    #274 - Added example for comments

---
+++ samples/tickets/getTicketComments.php
@@ -0,0 +1,29 @@
+<?php
+include("../../vendor/autoload.php");
+
+use Zendesk\API\HttpClient as ZendeskAPI;
+
+/**
+ * Replace the following with your own.
+ */
+
+ $subdomain = "subdomain";
+ $username  = "email@example.com";
+ $token     = "6wiIBWbGkBMo1mRDMuVwkw1EPsNkeUj95PIz2akv";
+
+$client = new ZendeskAPI($subdomain);
+$client->setAuth('basic', ['username' => $username, 'token' => $token]);
+
+try {
+    // Query the Zendesk API to retrieve the ticket comments
+
+      $id = 72;
+      $tickets = $client->tickets($id)->comments()->findAll();
+
+    // Show the results
+    echo "<pre>";
+    print_r($tickets);
+    echo "</pre>";
+} catch (\Zendesk\API\Exceptions\ApiResponseException $e) {
+    echo 'Please check your credentials. Make sure to change the $subdomain, $username, and $token variables in this file.';
+}
