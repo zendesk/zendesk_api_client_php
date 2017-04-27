3156063221a95a8a153921e90c8862655b60835d
Sam Gavinio <samgavinio@gmail.com>
2017-04-27 15:58:25 +0800

    Add samples for finding helpcenter articles and categories

---
+++ samples/helpcenter/findCategories.php
@@ -0,0 +1,27 @@
+<?php
+
+include("../../vendor/autoload.php");
+
+use Zendesk\API\HttpClient as ZendeskAPI;
+
+/**
+ * Replace the following with your own.
+ */
+
+$subdomain = "subdomain";
+$username  = "email@example.com";
+$token     = "6wiIBWbGkBMo1mRDMuVwkw1EPsNkeUj95PIz2akv";
+
+$client = new ZendeskAPI($subdomain);
+$client->setAuth('basic', ['username' => $username, 'token' => $token]);
+
+try {
+    // Find all helpcenter categories
+    $categories = $client->helpCenter->categories()->findAll();
+
+    echo "<pre>";
+    print_r($categories);
+    echo "</pre>";
+} catch (\Zendesk\API\Exceptions\ApiResponseException $e) {
+    echo $e->getMessage().'</br>';
+}
