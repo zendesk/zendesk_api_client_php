630c19d35baca43c1b9311ecd8fa54fa2ad61da7
Garrick Rohm <garrick.rohm@gmail.com>
2017-12-05 16:11:59 -0600

    reformatting for coding standard

--- samples/organizations/createOrganization.php
+++ samples/organizations/createOrganization.php
@@ -1,24 +1,26 @@
 <?php
 include("../../vendor/autoload.php");
 use Zendesk\API\HttpClient as ZendeskAPI;
+
 /**
  * Replace the following with your own.
  */
 $subdomain = "subdomain";
-$username  = "email@example.com";
-$token     = "6wiIBWbGkBMo1mRDMuVwkw1EPsNkeUj95PIz2akv";
+$username = "email@example.com";
+$token = "6wiIBWbGkBMo1mRDMuVwkw1EPsNkeUj95PIz2akv";
 $client = new ZendeskAPI($subdomain);
 $client->setAuth('basic', ['username' => $username, 'token' => $token]);
 try {
-	$newOrganization = $client->organizations()->create(
-	[
-    'external_id' => '123567854',
-    'name' => 'Example_Organization',
-    'shared_tickets' => 1,
-    'shared_comments' => 1,
-    'tags' => array('dog', 'cat', 'fish', 'strange_creature'),
-    'organization_fields' => array('favorite animal' => 'fish')
-    ]);
+    $newOrganization = $client->organizations()->create(
+        [
+        'external_id' => '123567854',
+        'name' => 'Example_Organization',
+        'shared_tickets' => 1,
+        'shared_comments' => 1,
+        'tags' => array('dog', 'cat', 'fish', 'strange_creature'),
+        'organization_fields' => array('favorite animal' => 'fish')
+        ]
+    );
     // Show result
     echo "<pre>";
     print_r($newOrganization);
