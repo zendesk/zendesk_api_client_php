b95e840bdb1134ebf83ae626d6a6b1c89aabb89b
Alvin See <alvin.see.hl@gmail.com>
2022-02-09 12:31:50 +1100

    add findAll endpoint

--- src/Zendesk/API/Resources/Core/Webhooks.php
+++ src/Zendesk/API/Resources/Core/Webhooks.php
@@ -23,6 +23,29 @@ class Webhooks extends ResourceAbstract
             'create'       => "{$this->resourceName}",
             'update'       => "{$this->resourceName}/{id}",
             'delete'       => "{$this->resourceName}",
+            'findAll'      => "{$this->resourceName}",
         ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findAll(array $params = [])
+    {
+        $queryParams = array_filter(array_flip($params), [$this, 'filterParams']);
+        $queryParams = array_merge($params, array_flip($queryParams));
+
+        return $this->traitFindAll($queryParams);
+    }
+
+    /**
+     * Filter parameters passed and only allow valid query parameters.
+     *
+     * @param $param
+     * @return int
+     */
+    private function filterParams($param)
+    {
+        return preg_match("/^sort|page[[a-zA-Z_]*]|filter[[a-zA-Z_]*](\\[\\]?)/", $param);
     }
 }
