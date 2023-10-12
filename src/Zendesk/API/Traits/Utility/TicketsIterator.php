362515b907f87f89321517f9a41080118f9c848d
Erik Trapin (ecoologic) <erik.trapin@zendesk.com>
2023-10-12 10:53:25 +1000

    Doc for TicketsIterator

--- src/Zendesk/API/Traits/Utility/TicketsIterator.php
+++ src/Zendesk/API/Traits/Utility/TicketsIterator.php
@@ -4,21 +4,57 @@ namespace Zendesk\API\Traits\Utility;
 
 use Iterator;
 
+/**
+ * An iterator for fetching tickets from the Zendesk API using cursor-based pagination.
+ */
 class TicketsIterator implements Iterator
 {
+    /**
+     * @var Zendesk\API\HttpClient The Zendesk API client.
+     */
     private $client;
+
+    /**
+     * @var int The current position in the tickets array.
+     */
     private $position = 0;
+
+    /**
+     * @var array The fetched tickets.
+     */
     private $tickets = [];
+
+    /**
+     * @var string|null The cursor for the next page of tickets.
+     */
     private $afterCursor = null;
+
+    /**
+     * @var int The number of tickets to fetch per page.
+     */
     private $pageSize;
+
+    /**
+     * @var bool A flag indicating whether the iterator has started fetching tickets.
+     */
     private $started = false;
 
+    // TODO: pageSize = 100
+    /**
+     * TicketsIterator constructor.
+     *
+     * @param Zendesk\API\HttpClient $client The Zendesk API client.
+     * @param int $pageSize The number of tickets to fetch per page.
+     */
     public function __construct($client, $pageSize = 2)
     {
         $this->client = $client;
         $this->pageSize = $pageSize;
     }
 
+    /**
+     * @return Ticket The current ticket, possibly fetching a new page.
+     */
     public function current()
     {
         if (!isset($this->tickets[$this->position]) && (!$this->started || $this->afterCursor)) {
@@ -27,21 +63,33 @@ class TicketsIterator implements Iterator
         return $this->tickets[$this->position];
     }
 
+    /**
+     * @return int The current position.
+     */
     public function key()
     {
         return $this->position;
     }
 
+    /**
+     * Moves to the next ticket.
+     */
     public function next()
     {
         ++$this->position;
     }
 
+    /**
+     * Rewinds to the first ticket.
+     */
     public function rewind()
     {
         $this->position = 0;
     }
 
+    /**
+     * @return bool True there is a current element after calls to `rewind` or `next`, possibly fetching a new page.
+     */
     public function valid()
     {
         if (!isset($this->tickets[$this->position]) && (!$this->started || $this->afterCursor)) {
@@ -50,6 +98,10 @@ class TicketsIterator implements Iterator
         return isset($this->tickets[$this->position]);
     }
 
+    // TODO: expose meta values
+    /**
+     * Fetches the next page of tickets from the API.
+     */
     private function getPage()
     {
         $this->started = true;
